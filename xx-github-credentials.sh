d1960d3b8f8f898fb9d0006f5feb97eac2253e68
Robert Michael Smith <nightwintertooth@gmail.com>
2022-08-21 05:56:33 -0700

    Update xx-github-credentials.sh

--- xx-github-credentials.sh
+++ xx-github-credentials.sh
@@ -4,7 +4,7 @@
 # SECURITY WARNING:  Do not intiiate a MPR with this file.  It has been added to .gitignore for this reason!  What has been provided is a template that you can
 #                    modify yourself.
 
-SSH_PASSPHRASE='TorrolfN2'
-GITHUB_EMAIL="nightwintertooth@gmail.com"
-GITHUB_USERNAME="loopyd"
+SSH_PASSPHRASE='your_email@example.com'
+GITHUB_EMAIL="your_enail@example.com"
+GITHUB_USERNAME="your_username"
 GITHUB_KEYFILE="$HOME/.ssh/id_tensorgit"
